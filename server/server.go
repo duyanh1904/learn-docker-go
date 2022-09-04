43e5bc298957c00bc3c3eeff5e40b7bba896b89e
anhnkd <duyanh3897@gmail.com>
2022-09-04 14:32:02 +0700

    change port

--- server/server.go
+++ server/server.go
@@ -1,9 +1,10 @@
 package server
 
-import "github.com/duyanh1904/learn-docker-go/config"
+import (
+	"os"
+)
 
 func Init() {
-	config := config.GetConfig()
 	r := NewRouter()
-	r.Run(config.GetString("server.port"))
+	r.Run(":" + os.Getenv("PORT"))
 }
