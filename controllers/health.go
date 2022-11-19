afa3d015545808644fe1397cf1ed3be8937a214b
anhnkd <@anh.nkd>
2022-11-19 13:51:45 +0700

    check health api

--- controllers/health.go
+++ controllers/health.go
@@ -2,6 +2,9 @@ package controllers
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/uptrace/opentelemetry-go-extra/otelplay"
+	"go.opentelemetry.io/contrib/instrumentation/github.com/gin-gonic/gin/otelgin"
+	"go.opentelemetry.io/otel/trace"
 	"net/http"
 )
 
@@ -9,4 +12,17 @@ type HealthController struct{}
 
 func (h HealthController) Status(c *gin.Context) {
 	c.String(http.StatusOK, "Working!")
+}
+
+const (
+	indexTmpl   = "index"
+	profileTmpl = "profile"
+)
+
+func ExampleMonitor(c *gin.Context) {
+	ctx := c.Request.Context()
+	otelgin.HTML(c, http.StatusOK, profileTmpl, gin.H{
+		"username": c.Param("username"),
+		"traceURL": otelplay.TraceURL(trace.SpanFromContext(ctx)),
+	})
 }
--- go.mod
+++ go.mod
@@ -8,8 +8,11 @@ require (
 	github.com/gin-gonic/gin v1.8.1
 	github.com/golang-jwt/jwt v3.2.2+incompatible
 	github.com/spf13/viper v1.12.0
-	github.com/stretchr/testify v1.8.1 // indirect
+	github.com/stretchr/testify v1.8.1
+	github.com/uptrace/opentelemetry-go-extra/otelplay v0.1.17
 	go.mongodb.org/mongo-driver v1.10.1
+	go.opentelemetry.io/contrib/instrumentation/github.com/gin-gonic/gin/otelgin v0.36.4
+	go.opentelemetry.io/otel/trace v1.11.1
 	gorm.io/driver/mysql v1.3.6
 	gorm.io/gorm v1.23.10
 )
